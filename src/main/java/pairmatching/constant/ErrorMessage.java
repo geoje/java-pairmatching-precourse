4018b6a24942f075f16af0718348b8a683e87e68
Gyeongho Yang <chchch1213@naver.com>
2023-12-15 22:19:02 +0900

    feat(const): 정보 제공이 얼마 없을 때의 오류 메세지 생성

--- src/main/java/pairmatching/constant/ErrorMessage.java
+++ src/main/java/pairmatching/constant/ErrorMessage.java
@@ -3,6 +3,8 @@ package pairmatching.constant;
 public enum ErrorMessage {
     EXCEPTION_PREFIX("[ERROR] "),
     MAIN_FUNC_NOT_FOUND("존재하지 않는 기능입니다. 다시 입력해 주세요."),
+
+    TOO_FEW_ARGUMENT("제공된 정보가 너무 적습니다. 다시 입력해 주세요."),
     COURSE_NOT_FOUND("존재하지 않는 과정입니다. 다시 입력해 주세요."),
     LEVEL_NOT_FOUND("존재하지 않는 레벨입니다. 다시 입력해 주세요."),
     MISSION_NOT_FOUND("해당 레벨에 존재하지 않는 미션입니다. 다시 입력해 주세요.");
