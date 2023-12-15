4d76b1855e311f6ecec3c7f260710eabca3d3586
Gyeongho Yang <chchch1213@naver.com>
2023-12-15 22:20:54 +0900

    feat(domain): 코스 미션 세트를 사용자 입력을 통해 가지고 오는 메서드 구현

--- src/main/java/pairmatching/domain/CourseMission.java
+++ src/main/java/pairmatching/domain/CourseMission.java
@@ -1,11 +1,26 @@
 package pairmatching.domain;
 
 import pairmatching.constant.Course;
+import pairmatching.constant.Level;
 import pairmatching.constant.Mission;
 
+import static pairmatching.constant.ErrorMessage.TOO_FEW_ARGUMENT;
+
 public record CourseMission(Course course, Mission mission) {
+    private static final String DELIMITER = ",";
+
+    private static void validateEnough(String choice) {
+        if (choice.split(DELIMITER).length < 3) {
+            throw new IllegalArgumentException(TOO_FEW_ARGUMENT.toString());
+        }
+    }
+
     public static CourseMission from(String choice) {
-        return new CourseMission(Course.BACKEND, Mission.RACING_CAR);
-        // TODO: Validate course, level, mission
+        validateEnough(choice);
+        String[] splitted = choice.split(DELIMITER);
+
+        return new CourseMission(
+                Course.fromName(splitted[0]),
+                Mission.fromLevelAndName(Level.fromName(splitted[1]), splitted[2]));
     }
 }
